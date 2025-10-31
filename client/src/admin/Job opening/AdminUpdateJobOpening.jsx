2a9c1d55ccd92cb2bf655f6cda0ecc98d2b3dab4
Preet Bhimani <preetpatel9472@gmail.com>
2025-10-31 22:04:19 +0530

    Implement Job Opening CRUD

--- client/src/admin/Job opening/AdminUpdateJobOpening.jsx
+++ client/src/admin/Job opening/AdminUpdateJobOpening.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
+import { useParams } from "react-router-dom";
 import UpdateJobOpening from "../../reusableComponent/Job Opening/UpdateJobOpening";
 
 const AdminUpdateJobOpening = () => {
 
     const [isCollapsed, setIsCollapsed] = useState(false);
+    const { id } = useParams();
 
     return <div className="flex flex-col h-screen">
 
@@ -22,7 +24,7 @@ const AdminUpdateJobOpening = () => {
                 <div className="text-center mt-8 mb-8">
                     <h1 className="text-4xl font-bold text-center text-white mb-4">Update Job Opening</h1>
                 </div>
-                <UpdateJobOpening />
+                <UpdateJobOpening id={id} />
             </main>
         </div>
 
--- client/src/recruiter/RecruiterUpdateJobOpening.jsx
+++ client/src/recruiter/RecruiterUpdateJobOpening.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import CommonNavbar from "../components/CommonNavbar";
 import RecruiterSidebar from "./RecruiterSidebar";
+import { useParams } from "react-router-dom";
 import UpdateJobOpening from "../reusableComponent/Job Opening/UpdateJobOpening";
 import Footer from "../components/Footer";
 
 function RecruiterUpdateJobOpening() {
 
     const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+    const { id } = useParams();
 
     return <div className="min-h-screen flex flex-col bg-neutral-950">
         {/* Navbar */}
@@ -22,7 +24,7 @@ function RecruiterUpdateJobOpening() {
                 <div className="text-center mt-8 mb-8">
                     <h1 className="text-4xl font-bold text-center text-white mb-4">Update Job Opening</h1>
                 </div>
-                <UpdateJobOpening />
+                <UpdateJobOpening id={id} />
             </main>
         </div>
         {/* Footer */}
